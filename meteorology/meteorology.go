b1cb27125e46767f569657ab2e5f4a945fc868a2
Nico <nmarlier84@yahoo.fr>
2026-07-22 15:31:36 +0200

    feat(stringers): done meteorology

---
+++ meteorology/meteorology.go
@@ -0,0 +1,63 @@
+package meteorology
+
+import "fmt"
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+// Add a String method to the TemperatureUnit type
+func (tu TemperatureUnit) String() string {
+	symb := []string{"°C", "°F"}
+	return symb[tu]
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+// Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit)
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+// Add a String method to SpeedUnit
+func (su SpeedUnit) String() string {
+	symb := []string{"km/h", "mph"}
+	return symb[su]
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+// Add a String method to Speed
+func (s Speed) String() string {
+	return fmt.Sprintf("%d %s", s.magnitude, s.unit)
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+// Add a String method to MeteorologyData
+func (md MeteorologyData) String() string {
+	// San Francisco: 57 °F, Wind NW at 19 mph, 60% Humidity
+	return fmt.Sprintf("%s: %s, Wind %s at %s, %d%% Humidity", md.location, md.temperature, md.windDirection, md.windSpeed, md.humidity)
+}
